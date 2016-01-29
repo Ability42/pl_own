4c1b257c41ee15a1071c326bfa6203c0303cde16
rikoover <extrafff@gmail.com>
2016-01-30 01:54:33 +0200

    1st

---
+++ basic.c
@@ -0,0 +1,40 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+char * open_file(); // func. prototype
+
+/*-----------------------------*/
+int main() {
+	char *p;
+
+	p = open_file();
+	for (int i = 0; i < 32; i++)
+		printf("%c", p[i]);
+	printf("\n");
+	return 0;
+}
+/*-----------------------------*/
+
+
+char * open_file(int argc, char *argv[1]) {
+	FILE *fp = fopen(argv[1], "r");
+	static char data[32];
+	if (fp == 0) {
+		printf("Can't open a file\n");
+	} else {
+		int x;
+		int i = 0;
+		while((x = fgetc(fp)) != EOF) {
+			i++;
+			data[i] = x;
+		}
+		fclose(fp);
+	}
+	printf("%s\n", argv[1]);
+	
+	return data;
+		
+} 
+
+
+
