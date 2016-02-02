c3537ed5c7a02b85bbc75b7f2f5572fc5e47d10a
rikoover <extrafff@gmail.com>
2016-02-02 23:52:54 +0200

    Crusial_remade

--- basic.c
+++ basic.c
@@ -1,78 +1,134 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <ctype.h>
 
-char * open_file(); 
-//void call(char * name); // makes a call to contact with `name`
-int lex(char *file_contents);
-/*-----------------------------------*/
-int main() {
 
-	char *p;
-	p = open_file();
-	lex(p);
+typedef enum { false = 0, true = 1} boolean;
 
-	return 0;
-}
-/*-----------------------------------*/
+#define NOV_func 2
+#define VOVV_func 5
+#define VOV_func 1
 
-// check syntax
-int lex(char *file_contents) { 
-	char * tok;
-	char to_srch[6] = "PRINT";
+// void func value
+char * sbasic_n_o_v_tokens[NOV_func] = {"PRINTN", "PRINTS"};
+// value func value value
+char * sbasic_v_o_vv_tokens[VOVV_func] = {"+", "-", "*", "/", "%"};
+// value func value
+char * sbasic_v_o_v_tokens[VOV_func] = {"SQUARE"};
 
-	int srch_pos = 0;
-	int text_pos = 0;
-	int len_srch = 5;
-	int len_text = 32;
-	for (int i = 0; i < 32; i++) 
-		tok[i] = file_contents[i];
 
-	for(text_pos = 0; text_pos < len_text - len_srch; ++text_pos) {
-		if(tok[text_pos] == to_srch[srch_pos]){
-			++srch_pos;
-			if(srch_pos == len_srch) {
-				//match
-				printf("match from %d to %d\n", text_pos - len_srch, text_pos);
+/*----------------------------------------------------------*/
+
+FILE *openFile(const char * name);
+void throwError(char * msg, int code);
+//boolean isEmpty(FILE * fp);
+void nextToken(FILE * fp,char * token, boolean * isEOF);
+boolean isOperation(char * token);
+void tokenToString(char * token, char * word);
+
+
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+int main(int argc, char const * argv[]) {
+
+	FILE * fp = openFile(argv[1]);
+	boolean isEOF = false;
+	char token[100];
+
+	while(!isEOF) {
+		nextToken(fp, token, &isEOF);
+		if(!isEOF) {
+			if (isOperation(token)) {
+				if (strcmp(token, "PRINTS") == 0) {
+					nextToken(fp, token, &isEOF);
+					char str[100];
+					tokenToString(token, str);
+					puts(str);
+				} else if (strcmp(token, "PRINTN") == 0) {
+					printf("%lf\n", parse_arithmetic(fp));
+				}
 			}
-		} else {
-			text_pos -= srch_pos;
-			srch_pos = 0;
 		}
 	}
-	//no match
-	printf("no match\n");
-	/*if (strstr(tok, needle) != NULL)
-		printf("Found a PRINT!\n");
-	printf("lol\n");*/
-	return 0;
-} 
-
-int search() {
-	
 
 	return 0;
 }
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
 
-char * open_file(int argc, char *argv[1]) {
-	FILE *fp = fopen(argv[1], "r");
-	static char data[32];
-	if (fp == 0) {
-		printf("Can't open a file\n");
+FILE * openFile(const char * name) {
+
+	FILE * fp = fopen(name, "r");
+	if (!fp) {
+		throwError("Cannot open file\n", 101);
 	} else {
-		int x;
-		int i = 0;
-		while((x = fgetc(fp)) != EOF) {
-			i++;
-			data[i] = x;
-		}
-		fclose(fp);
+		printf("Open Success\n");
 	}
-	printf("%s\n", argv[1]);
-	
-	return data;
-		
+	return fp;
 }
 
+void throwError(char * msg, int code) {
+	printf("ERROR %d : %s", code, msg);
+	exit(code);
+}
 
+void nextToken(FILE * fp, char * token, boolean * isEOF) {
+	int xs = fscanf(fp, "%s", token);
+	if (xs == EOF) {
+		*isEOF = true;
+	} else {
+		*isEOF = false;
+	}
+}
 
+boolean isOperation(char * token) {
+	for (int i = 0; i < NOV_func; ++i) {
+		if (strcmp(token, sbasic_n_o_v_tokens[i]) == 0) {
+			return true;
+		}
+	}
+	
+	for (int i = 0; i < VOV_func; ++i) {
+		if (strcmp(token, sbasic_v_o_v_tokens[i]) == 0) {
+			return true;
+		}
+	}
+	
+	for (int i = 0; i < VOVV_func; ++i) {
+		if (strcmp(token, sbasic_v_o_vv_tokens[i]) == 0) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+void tokenToString(char * token, char * word) {
+	substring(token, word, 2, (int)strlen(token) - 2)
+}
+
+void substring(char s[], char sub[], int p, int l) {
+	int c = 0;
+
+	while (c < l) {
+		sub[c] = s[p+c-1];
+		c++;
+	}
+	sub[c] = '\0'
+}
+
+double parse_arithmetic(FILE *fp) {
+	boolean isEOF;
+	char token[100];
+
+	nextToken(fp,token,&isEOF);
+}
+/*boolean isEmpty(FILE * fp) {
+	if (((int)strlen(fp)) == NULL) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}*/
