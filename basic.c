4e51022bff5d85bf507ba70d8bae22cc13843a69
rikoover <extrafff@gmail.com>
2016-02-09 18:35:37 +0200

    no message

--- basic.c
+++ basic.c
@@ -16,7 +16,7 @@ char * sbasic_n_o_v_tokens[NOV_func] = {"PRINTN", "PRINTS"};
 char * sbasic_v_o_vv_tokens[VOVV_func] = {"+", "-", "*", "/", "%"};
 // value func value
 char * sbasic_v_o_v_tokens[VOV_func] = {"SQUARE"};
-
+char * sbasic_type_tokens[2] = {"String", "Number"};
 
 /*----------------------------------------------------------*/
 
@@ -26,6 +26,12 @@ void throwError(char * msg, int code);
 void nextToken(FILE * fp,char * token, boolean * isEOF);
 boolean isOperation(char * token);
 void tokenToString(char * token, char * word);
+void substring(char s[], char sub[], int p, int l);
+double parse_arithmetic(FILE *fp);
+boolean isNumber(char * token);
+double tokenToNumber(char * token);
+boolean isStringLiteral(char * token);
+
 
 
 /*----------------------------------------------------------*/
@@ -36,7 +42,7 @@ int main(int argc, char const * argv[]) {
 	boolean isEOF = false;
 	char token[100];
 
-	while(!isEOF) {
+	do {
 		nextToken(fp, token, &isEOF);
 		if(!isEOF) {
 			if (isOperation(token)) {
@@ -50,7 +56,8 @@ int main(int argc, char const * argv[]) {
 				}
 			}
 		}
-	}
+			printf("%s : is number : %d, is string : %d\n", token, isNumber(token), isStringLiteral(token));
+	} while(!isEOF);
 
 	return 0;
 }
@@ -71,6 +78,32 @@ FILE * openFile(const char * name) {
 void throwError(char * msg, int code) {
 	printf("ERROR %d : %s", code, msg);
 	exit(code);
+}
+
+boolean isStringLiteral(char * token) {
+	int start = 0;
+	int end = (int)strlen(token);
+
+	for (int i = start; i <= end; ++i) {
+		if (i == start) {
+			if (token[i] != '"')
+				return false;
+			else
+				continue;
+		}
+
+		if (i == end) {
+			if (token[i] != '"')
+				return false;
+			else
+				continue;
+		}
+		if (token[i] == '"') {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 void nextToken(FILE * fp, char * token, boolean * isEOF) {
@@ -105,7 +138,7 @@ boolean isOperation(char * token) {
 }
 
 void tokenToString(char * token, char * word) {
-	substring(token, word, 2, (int)strlen(token) - 2)
+	substring(token, word, 2, (int)strlen(token) - 2);
 }
 
 void substring(char s[], char sub[], int p, int l) {
@@ -115,7 +148,7 @@ void substring(char s[], char sub[], int p, int l) {
 		sub[c] = s[p+c-1];
 		c++;
 	}
-	sub[c] = '\0'
+	sub[c] = '\0';
 }
 
 double parse_arithmetic(FILE *fp) {
@@ -123,6 +156,37 @@ double parse_arithmetic(FILE *fp) {
 	char token[100];
 
 	nextToken(fp,token,&isEOF);
+
+	if (isNumber(token)) {
+		return tokenToNumber(token);
+	} else if (isOperation(token)) {
+		if (strcmp(token, "+") == 0) {
+			return (parse_arithmetic(fp) + parse_arithmetic(fp));
+		} else if (strcmp(token, "-") == 0) {
+			return (parse_arithmetic(fp) - parse_arithmetic(fp));
+		} else if (strcmp(token, "SQUARE")) {
+			double num = parse_arithmetic(fp);
+			return num = num*num;
+		} else {
+			throwError("Token isn't a number or arithmetic operator", 202);
+			return -1;
+		} 
+	} else {
+		throwError("Token isn't number or arithmetic operation", 202);
+		return -1;
+	}
+}
+
+double tokenToNumber(char * token) {
+	return atof(token);
+}
+
+boolean isNumber(char * token) {
+	double res = atof(token);
+
+	boolean isZero = strlen(token) == 1 && token[0] == '0';
+
+	return (res == 0) ? isZero : true;
 }
 /*boolean isEmpty(FILE * fp) {
 	if (((int)strlen(fp)) == NULL) {
